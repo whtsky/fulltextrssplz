be43e4264cc9fad018e614a4f11401a2c228d0a7
Wu Haotian <whtsky@gmail.com>
2020-06-30 00:37:43 +0800

    build: add purgecss

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+// @ts-ignore
+import postcss from 'rollup-plugin-postcss'
+import purgecss from '@fullhuman/postcss-purgecss'
 
 export default {
   input: 'src/frontend.ts',
@@ -18,6 +21,17 @@ export default {
       module: 'es6',
       target: 'es5',
     }),
+    postcss({
+      extract: 'styles.css',
+      plugins: [
+        purgecss({
+          content: ['./public/*.html'],
+        }),
+      ],
+    }),
     terser(),
+  ],
+  external: [
+    'crypto', // not used in frontend code
   ],
 }
--- src/frontend.ts
+++ src/frontend.ts
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { sign } from './sign'
 
 const urlInput = document.getElementById('url') as HTMLInputElement
@@ -11,10 +13,13 @@ keyInput.addEventListener('input', () => updateUrl())
 
 function updateUrl() {
   const url = urlInput.value
-  const key = keyInput.value
-  let targetUrl = `${location.origin}/feed?url=${url}&format=${formatInput.value}`
-  if (key) {
-    targetUrl += `&sign=${sign(url, key)}`
+  let targetUrl = ''
+  if (url) {
+    const key = keyInput.value
+    targetUrl = `${location.origin}/feed?url=${url}&format=${formatInput.value}`
+    if (key) {
+      targetUrl += `&sign=${sign(url, key)}`
+    }
   }
   urlOutput.innerText = targetUrl
   urlOutput.href = targetUrl
