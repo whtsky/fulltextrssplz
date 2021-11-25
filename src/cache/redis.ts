3c3d20e780d03a1c821b7e366400722719f69a43
whtsky <whtsky@gmail.com>
2021-11-25 19:38:25 +0800

    feat: lazy load redis

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -1,31 +1,43 @@
 import type { Cache } from './type'
-import redis from "redis"
-
+import type { RedisClient } from 'redis'
 
 export class RedisCache implements Cache {
-  client: redis.RedisClient;
-  ttl: number|undefined;
+  redisUrl: string
+  client?: RedisClient
+  ttl: number | undefined
   constructor(url: string, ttl: number | undefined = undefined) {
-    this.client = redis.createClient({ url });
+    this.redisUrl = url
     this.ttl = ttl
   }
+  private async ensureClient() {
+    if (!this.client) {
+      const redis = await import('redis')
+      this.client = redis.createClient({ url: this.redisUrl })
+    }
+  }
   get(url: string) {
-    return new Promise<string | undefined>((resolve, reject) => {
-      this.client.get(url, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data || undefined)
-        }
+    return new Promise<string | undefined>(async (resolve, reject) => {
+      try {
+        await this.ensureClient()
+        this.client!.get(url, (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(data || undefined)
+          }
+        })
+      } catch (e) {
+        reject(e)
+      }
     })
-  })
-}
+  }
 
-  set(url: string, content: string): Promise<void> {
+  async set(url: string, content: string) {
+    await this.ensureClient()
     if (this.ttl !== undefined) {
-      this.client.set(url, content, 'EX', this.ttl)
+      this.client!.set(url, content, 'EX', this.ttl)
     } else {
-      this.client.set(url, content)
+      this.client!.set(url, content)
     }
     return Promise.resolve()
   }
