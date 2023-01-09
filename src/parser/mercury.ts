0071b68af9a3e77816255a0a8686bed27e8670af
whtsky <whtsky@gmail.com>
2023-01-09 20:05:45 +0800

    chore: upgrade dependencies

--- src/parser/mercury.ts
+++ src/parser/mercury.ts
@@ -1,6 +1,6 @@
 import type { ParsedPage } from './types'
 
-import Mercury from '@postlight/mercury-parser'
+import Mercury from '@postlight/parser'
 
 export function parsePageUsingMercury(url: string): Promise<ParsedPage> {
   return Mercury.parse(url).then((result: { [x: string]: any }) => {
